b597a1c81d48f4e8735e9be03da9854aa4fe4741
hayshin <hayshinbj@gmail.com>
2025-09-08 20:55:43 +0500

    chore: init commit

---
+++ wordcount/src/main/java/org/example/WordCountMapper.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper
+    extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private static final IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException {
+        String line = value.toString();
+        for (String token : line.split("\\s+")) {
+            word.set(token);
+            context.write(word, one);
+        }
+    }
+}
